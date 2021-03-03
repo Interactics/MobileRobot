70fc3274df9a90996ae2036eef401dbebc71a762
yuhoyeon <zz31@naver.com>
2021-03-03 12:00:41 +0900

    add Differential Drive Mobile Robot class

--- include/mobilerobot/DifferentialDrive.h
+++ include/mobilerobot/DifferentialDrive.h
@@ -12,16 +12,28 @@
 #define __DIFF_DRIVE_H__
 
 #include "robot.h"
+
+#define LEFT   0
+#define RIGHT  1
+
 namespace mobile{
-class DiffDrive : public MobileRobot{
+
+class DiffDrive : public MobileRobot {
 private:
-    int ActuatorNum;
-    int WheelSize;
-    int TrackSize;
-    int WheelBase;
+    int   actNum;
+    float wheelDiameter;
+    float trackDist;
+    float wheelBase;
 public:
-    DiffDrive(int ActNum, int WheelDiameter, int Trackdist);
-    
+    DiffDrive(const int actNum, const float wheelDiameter, const float trackDist)
+    :actNum(actNum), wheelDiameter(wheelDiameter), trackDist(trackDist), wheelBase(0) {};
+    ~DiffDrive(){}
+
+    void setActNum(const int actNum)                 { this -> actNum = actNum; }
+    void setWheelDiameter(const float wheelDiameter) { this -> wheelDiameter = wheelDiameter; } 
+    void setTrackDist(const float trackDist)         { this -> trackDist = trackDist; } 
+
+    bool MotorCtrl(float* velocity);
 }; // class
 }
 #endif // __DIFF_DRIVE_H__
