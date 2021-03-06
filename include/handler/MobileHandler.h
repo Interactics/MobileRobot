9698680ed6b3e72df00e2112153f139cacef11d0
interactics <zz31@naver.com>
2021-03-07 02:21:32 +0900

    Added mobile robot handler class

---
+++ include/handler/MobileHandler.h
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * DifferentialDrive.h
+ * Date 2021.03.07
+ * Created by Interatics
+ *******************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////////
+/// @file The file for Mobile Robot Handler
+/// @author Hoyeon Yu (Interactics)
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __MOBILEHANDLER_H__
+#define __MOBILEHANDLER_H__
+
+namespace mobile {
+    
+} // namespace mobile
+
+
+#endif // __MOBILEHANDLER_H__
